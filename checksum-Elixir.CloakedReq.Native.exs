765a3972c8044163b48db98ecc5e02adee3f447d
Onni Hakala <onni@flaky.build>
2026-04-08 15:01:32 +0300

    build: regenerate checksum for versioned-filename NIF tarball

--- checksum-Elixir.CloakedReq.Native.exs
+++ checksum-Elixir.CloakedReq.Native.exs
@@ -1,3 +1,3 @@
 %{
-  "libcloaked_req_native-v0.3.2-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d855c4639c076220fa812eb79e512ebdb6b495b39b85a177bc3276861b247999",
+  "libcloaked_req_native-v0.3.2-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:cf68ad89833260a75f77e2f9b7ccbae329e74a9049236028fcb7673ae6fcb7d7",
 }
