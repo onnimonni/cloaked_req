dc446d90d078ac2cebcac77ed459612a25ec876c
Onni Hakala <onni@flaky.build>
2026-04-08 14:55:51 +0300

    build: update checksum for custom x86_64-linux NIF build

--- checksum-Elixir.CloakedReq.Native.exs
+++ checksum-Elixir.CloakedReq.Native.exs
@@ -1,5 +1,3 @@
 %{
-  "libcloaked_req_native-v0.3.2-nif-2.17-aarch64-apple-darwin.dylib.tar.gz" => "sha256:0803635a7b76b707ae979ebe66a6c003201dd27874b74872932d501f461f634a",
-  "libcloaked_req_native-v0.3.2-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:384ba6cbfcf80c64d654b091ae0aa3a3a37be2dc96fb3e800d6f0353fe6269a6",
-  "libcloaked_req_native-v0.3.2-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:71e62d42d72822bdd26b9a2d5b0a746783754e546639231ef0aff9921392b3ba",
+  "libcloaked_req_native-v0.3.2-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d855c4639c076220fa812eb79e512ebdb6b495b39b85a177bc3276861b247999",
 }
